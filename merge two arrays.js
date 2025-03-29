0e30fa73298650e417a997d419fb05d1e9b89164
wiktor <wiktorlem1@wp.pl>
2025-03-29 04:28:50 +0100

    leetcode problem 88 Merge Sorted Array started

---
+++ merge two arrays.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/merge-sorted-array/
+function merge(nums1, m, nums2, n){
+
+    let len = nums1.length;
+    for (let i=len+1;i<=m;i--){
+        console.log(nums1[i])
+    }
+}
+//merge([1,2,3,0,0,0], 3, [1,2,3], 3)
+merge([1], 1, [], 0 )
